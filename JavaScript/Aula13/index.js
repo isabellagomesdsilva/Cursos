085ec753b053d96644de319fc74a32678491c3d3
Isabella Gomes <gsilvaaisa@gmail.com>
2023-01-02 17:07:25 -0300

    new file:   JavaScript/Aula13/index.js

---
+++ JavaScript/Aula13/index.js
@@ -0,0 +1,51 @@
+/* 
+Operadores de comparação
+
+> Maior que
+>= Maior ou igual a
+< Menor que
+<= Menor ou igual a
+== Igual a (checa apenas o valor) = em desuso
+=== Igual e do mesmo tipo (checa o valor e o tipo)
+!= Diferente de (checa apenas o valor) = em desuso
+!== Diferente, inclusive do tipo (checa o valor e o tipo)
+*/
+
+/* 
+Operadores lógicos
+
+&& - AND - "E" Todas as condições devem ser verdadeiras para que a condição final seja verdadeira.
+|| - OR - "OU" Uma das condições deve ser verdadeira para que a condição final seja verdadeira.
+! - NOT - "NÃO" Nega uma condição.
+
+Valores que retornam false:
+- false = único falso literal
+- 0
+- '' "" ``
+- null / undefined
+- NaN
+*/
+
+/*
+Operadores condicionais
+
+O if pode ser usado sozinho ou com o else ou com o else if, só que o else sempre precisa de um if antes.
+Pode existir vários else if, mas só um else.
+
+if (condição tem que ser verdadeira) {
+    // código a ser executado
+} else (caso a condição do if não seja verdadeira){
+    // código a ser executado
+}
+
+if (condição tem que ser verdadeira) {
+    // código a ser executado
+} else if (checa outra condição) {
+    // código a ser executado
+} else {
+    // código a ser executado
+}
+*/
+
+
+
